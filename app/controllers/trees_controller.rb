fe461c1bf2666fba758845b57185811bc5c7f18d
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-06-08 22:11:53 -0400

    added location to autocomplete

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -6,14 +6,19 @@ class TreesController < ApplicationController
   
   def create
     @word = params["query"]
+    @iso = params["iso_code"]
     @root = params["query"].split(" ").first
-    tree = Tree.new(tree_params)
-    @array = tree.auto
-    @legit = eval(@array)[1]
+    tree = Tree.new(@word, @iso)
+    @hash = tree.auto
+    @legit = []
+    @hash["toplevel"]["CompleteSuggestion"].each do |hash|
+       @legit << hash["suggestion"]["data"]
+    end 
+    # @legit = eval(@array)[1]
   end 
 
-  private
-  def tree_params
-    params.require(:query)
-  end 
+  # private
+  # def tree_params
+  #   # params.require(:query, :iso_code)
+  # end 
 end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,13 +1,19 @@
 require 'open-uri'
+require 'nokogiri'
 class Tree < ActiveRecord::Base
 
-  def initialize(word)
+  def initialize(word, iso)
+    @iso = iso
     @word = word
   end 
   
   def auto
-    uri = URI.parse(URI.encode("http://suggestqueries.google.com/complete/search?client=firefox&q=#{@word}").strip)
-    uri.read
+    # uri = URI.parse(URI.encode("http://suggestqueries.google.com/complete/search?client=firefox&q=#{@word}&gl=#{@iso}").strip)
+    uri = URI.parse(URI.encode("http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=#{@word}&gl=#{@iso}").strip)
+    # uri=URI.parse('google.com/complete/search?output=toolbar&q=theory&gl=in')
+    xml = uri.read
+    response = Crack::XML.parse(xml)
+    # uri.read
   end 
 
 end
