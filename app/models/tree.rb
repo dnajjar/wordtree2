82902889d1e29eda542dcc1667c467f87bc09674
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-06-09 00:02:23 -0400

    remove comments

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -8,12 +8,9 @@ class Tree < ActiveRecord::Base
   end 
   
   def auto
-    # uri = URI.parse(URI.encode("http://suggestqueries.google.com/complete/search?client=firefox&q=#{@word}&gl=#{@iso}").strip)
     uri = URI.parse(URI.encode("http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=#{@word}&gl=#{@iso}").strip)
-    # uri=URI.parse('google.com/complete/search?output=toolbar&q=theory&gl=in')
     xml = uri.read
     response = Crack::XML.parse(xml)
-    # uri.read
   end 
 
 end
