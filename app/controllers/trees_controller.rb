1c6e0ea259635d747cae218b63dfd54b1afd68f2
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-06-17 11:18:16 -0400

    added random button

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -5,37 +5,57 @@ class TreesController < ApplicationController
   def create
 
     @word = params["query"]
-    @auto1 =get_auto_from_query(location1, @word)
+    # @auto1 =get_auto_from_query(location1, @word)
 
-    # @iso1=params["location1"].split(", ")[0]
-    # @location1 = params["location1"].split(", ")[1]
-    # tree1 = Tree.new(@word, @iso1)
-    # @auto1 = tree1.auto
+    @iso1=params["location1"].split(", ")[0]
+    @location1 = params["location1"].split(", ")[1]
+    tree1 = Tree.new(@word, @iso1)
+    @auto1 = tree1.auto
     # @hash1 = tree1.auto
     # @auto1 = []
     
     # @hash1["toplevel"]["CompleteSuggestion"].each do |hash|
     #    @auto1 << hash["suggestion"]["data"]
     # end  
+      if params["location2"]
+      unless params["location2"]=="none"
+        # @auto2=get_auto_from_query(location2, @word)
+        @iso2=params["location2"].split(", ")[0]
+        @location2 = params["location2"].split(", ")[1]
+        tree2 = Tree.new(@word, @iso2)
+        @auto2 = tree2.auto
+        # # @auto2 = []
+        # @hash2["toplevel"]["CompleteSuggestion"].each do |hash|
+        #   @auto2 << hash["suggestion"]["data"]
+        # end 
+        @auto3 = Tree.overlap(@auto1,@auto2)
 
-    unless params["location2"]=="none"
-      @auto2=get_auto_from_query(location2, @word)
-      # @iso2=params["location2"].split(", ")[0]
-      # @location2 = params["location2"].split(", ")[1]
-      # tree2 = Tree.new(@word, @iso2)
-      # @auto2 = tree2.auto
-      # # @auto2 = []
-      # @hash2["toplevel"]["CompleteSuggestion"].each do |hash|
-      #   @auto2 << hash["suggestion"]["data"]
-      # end 
-      @auto3 = Tree.overlap(@auto1,@auto2)
-
-    end 
+      end 
+    end
 
     @root = params["query"].split(" ").first
 
   end 
 
+  def random
+    @word = ["who","what", "where is", "when", "why", "how many times"].sample
+    @country1 = ["us, United States", "eg, Egypt", "kr, South-Korea", "sa, Saudi-Arabia", "in, India"].sample 
+    @country2 =  @country1
+      while @country2 == @country1
+    @country2 = ["us, United States", "eg, Egypt", "kr, South-Korea", "sa, Saudi-Arabia", "in, India"].sample 
+      end 
+    @iso1 = @country1.split(", ")[0]
+    @iso2 = @country2.split(", ")[0]
+    @location1 = @country1.split(", ")[1]
+    @location2 = @country2.split(", ")[1]
+    tree1 = Tree.new(@word, @iso1)
+    @auto1 = tree1.auto
+    tree2 = Tree.new(@word, @iso2)
+    @auto2 = tree2.auto
+    @auto3 = Tree.overlap(@auto1,@auto2)
+    @root = @word.split(" ").first
+
+  end 
   def get_auto_from_query(location, word)
 
     @iso1=params["#{location}"].split(", ")[0]
